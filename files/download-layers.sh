e28c6d61a0dc0b3596aa49bd18e83fa17af5c833
Jochen Klar <jochenklar@gmail.com>
2015-07-06 20:46:50 +0200

    layers and apache deployment

---
+++ files/download-layers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /srv/gis/layers
+wget tiles.codefor.de/static/bbs-shp.tar.bz2
+
+tar xjvf bbs-shp.tar.bz2
--- files/wsgi.py
+++ files/wsgi.py
@@ -2,5 +2,3 @@ import os
 import TileStache
 
 application = TileStache.WSGITileServer('/srv/gis/tilestache/tilestache.cfg')
-
-service apache2 restart
